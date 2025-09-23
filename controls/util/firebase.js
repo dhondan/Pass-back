1199ccd9328e87a0d5b6eb92ba2ec754e32ffeac
dhondan <danielsouza13579582@gmail.com>
2025-09-23 19:21:31 -0300

    a

--- controls/util/firebase.js
+++ controls/util/firebase.js
@@ -1,10 +1,12 @@
-var admin = require("firebase-admin");
+import admin from "firebase-admin";
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+// Pega a configuração do Firebase a partir da variável de ambiente
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG || "{}");
 
-initializeApp({
-  credential: _credential.cert(serviceAccount),
-  databaseURL: "https://projeto-a99a9-default-rtdb.firebaseio.com"
+// Inicializa o app do Firebase
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://projeto-a99a9-default-rtdb.firebaseio.com",
 });
 
 export const app = admin;
