6b7181d66173f71949ee01756375844e7f8b3cb5
dhondan <danielsouza13579582@gmail.com>
2025-09-27 16:25:24 -0300

    correção do logout

--- controls/UsersRouter/Sair.js
+++ controls/UsersRouter/Sair.js
@@ -1,6 +1,15 @@
 export async function Logout(server, opts) {
   server.get('/logout', async (request, reply) => {
-    reply.clearCookie("token", { path: "/" }).send({ message: "Logout realizado" });
-    console.log("saiu")
+    reply
+      .setCookie("token", "", {
+        httpOnly: true,
+        secure: true,     // se estiver em HTTPS
+        sameSite: "none",  // ou 'none' se for cross-site
+        path: "/",
+        maxAge: 0         // expira imediatamente
+      })
+      .send({ message: "Logout realizado" });
+
+    console.log("saiu");
   });
 }
