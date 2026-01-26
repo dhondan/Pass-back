dc66342350c1a65b05a9c7859eeb827653817b7b
dhondan <danielsouza13579582@gmail.com>
2026-01-26 20:20:18 -0300

    adição de valor crítico em .env

--- controls/util/firebase.js
+++ controls/util/firebase.js
@@ -1,4 +1,5 @@
 import admin from "firebase-admin";
+import dotenv from "dotenv";
 
 // Pega a configuração do Firebase a partir da variável de ambiente
 const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
@@ -6,7 +7,7 @@ const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 // Inicializa o app do Firebase
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://projeto-a99a9-default-rtdb.firebaseio.com",
+  databaseURL: process.env.FIREBASE_URL,
 });
 
 export const app = admin;
